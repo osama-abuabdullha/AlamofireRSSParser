1bee0d43f83370e47dd22cd28768bf7af76e08d1
Osama Ramdan <88005233+osama-abuabdullha@users.noreply.github.com>
2022-07-07 10:26:05 +0300

    support image

--- Pod/Classes/RSSFeed.swift
+++ Pod/Classes/RSSFeed.swift
@@ -19,6 +19,7 @@ import Foundation
 open class RSSFeed: CustomStringConvertible {
     open var title: String? = nil
     open var link: String? = nil
+    open var image: String? = nil
     open var feedDescription: String? = nil
     open var pubDate: Date? = nil
     open var lastBuildDate: Date? = nil
@@ -33,7 +34,7 @@ open class RSSFeed: CustomStringConvertible {
     open var items: [RSSItem] = Array()
     
     open var description: String {
-        return "title: \(String(describing: self.title))\nfeedDescription: \(String(describing: self.feedDescription))\nlink: \(String(describing: self.link))\npubDate: \(String(describing: self.pubDate))\nlastBuildDate: \(String(describing: self.lastBuildDate))\nlanguage: \(String(describing: self.language))\ncopyright: \(String(describing: self.copyright))\nmanagingEditor: \(String(describing: self.managingEditor))\nwebMaster: \(String(describing: self.webMaster))\ngenerator: \(String(describing: self.generator))\ndocs: \(String(describing: self.docs))\nttl: \(String(describing: self.ttl))\nitems: \n\(self.items)"
+        return "title: \(String(describing: self.title))\nimage: \(String(describing: self.image))\nfeedDescription: \(String(describing: self.feedDescription))\nlink: \(String(describing: self.link))\npubDate: \(String(describing: self.pubDate))\nlastBuildDate: \(String(describing: self.lastBuildDate))\nlanguage: \(String(describing: self.language))\ncopyright: \(String(describing: self.copyright))\nmanagingEditor: \(String(describing: self.managingEditor))\nwebMaster: \(String(describing: self.webMaster))\ngenerator: \(String(describing: self.generator))\ndocs: \(String(describing: self.docs))\nttl: \(String(describing: self.ttl))\nitems: \n\(self.items)"
     }
     
 }
